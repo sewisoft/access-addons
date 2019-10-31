daf14d754bc11ab42fd7a86df38735b91e533d01
Günter Selbert <guenter.selbert@sewisoft.de>
2019-10-31 15:49:29 +0100

    [FIX] fields_get context wrong argument

--- access_settings_menu/models.py
+++ access_settings_menu/models.py
@@ -1,10 +1,13 @@
-from openerp import models, api
+from odoo import models, api
 
 
 class ResUsers(models.Model):
     _inherit = 'res.users'
 
     @api.model
-    def fields_get(self, *args, **kwargs):
-        # switch to superuser to get access to virtual fields
-        return super(ResUsers, self.sudo()).fields_get(*args, **kwargs)
+    def fields_get(self, fields=None, attributes=None):
+        """ If an addon is already installed, set it to readonly as
+        res.config.installer doesn't handle uninstallations of already
+        installed addons
+        """
+        return super(ResUsers, self.sudo()).fields_get(fields, attributes=attributes)
